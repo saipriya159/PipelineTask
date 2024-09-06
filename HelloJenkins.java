b6e67f29654aba00d76a8281830c164957b6df25
Jyothisri <Gollajyothisri6@gmail.com>
2024-09-06 16:33:21 +0530

    added 3 files

---
+++ HelloJenkins.java
@@ -0,0 +1,7 @@
+public class HelloJenkins
+{
+public static void main(String[] args)
+{
+System.out.println("Hello Jenkins");
+}
+}
---
+++ HelloWipro.java
@@ -0,0 +1,7 @@
+public class HelloWipro
+{
+public static void main(String[] args)
+{
+System.out.println("Hello Wipro");
+}
+}
